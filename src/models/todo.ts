f699d1ba249550282d5578ab05fd2b17473621f9
Jorerock <jorerock@laposte.net>
2024-09-23 17:24:55 +0200

    17h24

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -2,6 +2,7 @@
  * On définit une interface qui représente notre modèle de données
  * (la structure que nous manipulons pour représenter une entité)
  */
+
 export interface Todo {
   id: number;
   label: string;
