e64078246de5f44340ffbb7f1dbfb6d1507c65dc
Jorerock <jorerock@laposte.net>
2024-09-23 15:16:52 +0200

    15h16

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -52,3 +52,47 @@ const onCancelText = () => {
     color: green;
 }
 </style>
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Todo } from '../models/Todo'; // Import de l'interface
+Todo
+const props = defineProps<{
+todo: Todo;
+}>();
+const editMode = ref(false);
+const newValue = ref(props.todo.label); // Utilisation de "label"
+au lieu de "todo"
+const emit = defineEmits(['onInput']);
+const onInput = (value: boolean) => {
+console.log('TodoComponent a détecté un changement', value);
+emit('onInput', { ...props.todo, done: value });
+};
+const onConfirmText = () => {
+editMode.value = false;
+emit('onInput', { ...props.todo, label: newValue.value });
+};
+const onCancelText = () => {
+editMode.value = false;
+newValue.value = props.todo.label;
+};
+</script>
+
+<template>
+    <span v-if="!editMode">
+    <span @click="editMode = !editMode">
+    {{ props.todo.label }}
+    </span>
+    <input type="checkbox" :checked="props.todo.done" @click="(event:
+    any) => onInput(event.target?.checked)" />
+    <br />
+    </span>
+    <span v-else>
+    <!-- mode édition -->
+    <input type="text" v-model="newValue" />
+    <button @click="onConfirmText">Confirmer</button>
+    <button @click="onCancelText">Annuler</button>
+    <br />
+    </span>
+    </template>
